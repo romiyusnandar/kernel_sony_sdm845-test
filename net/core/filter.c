0a0c8f99cc4350e6f025e98aa6e202d9e9a30ae5
John Fastabend <john.fastabend@gmail.com>
2017-10-18 07:10:58 -0700

    bpf: remove mark access for SK_SKB program types
    The skb->mark field is a union with reserved_tailroom which is used
    in the TCP code paths from stream memory allocation. Allowing SK_SKB
    programs to set this field creates a conflict with future code
    optimizations, such as "gifting" the skb to the egress path instead
    of creating a new skb and doing a memcpy.
    
    Because we do not have a released version of SK_SKB yet lets just
    remove it for now. A more appropriate scratch pad to use at the
    socket layer is dev_scratch, but lets add that in future kernels
    when needed.
    
    Change-Id: I96fc1388ff30111f7365d960948ede6895c22f6c
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit f7e9cb1ecb6d922584abff16db07930162c57155)
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- net/core/filter.c
+++ net/core/filter.c
@@ -3651,7 +3651,20 @@ static bool sk_skb_is_valid_access(int off, int size,
 				   enum bpf_access_type type,
 				   struct bpf_insn_access_aux *info)
 {
+	if (type == BPF_WRITE) {
+		switch (off) {
+		case bpf_ctx_range(struct __sk_buff, tc_index):
+		case bpf_ctx_range(struct __sk_buff, priority):
+			break;
+		default:
+			return false;
+		}
+	}
+
 	switch (off) {
+	case bpf_ctx_range(struct __sk_buff, mark):
+	case bpf_ctx_range(struct __sk_buff, tc_classid):
+		return false;
 	case bpf_ctx_range(struct __sk_buff, data):
 		info->reg_type = PTR_TO_PACKET;
 		break;
